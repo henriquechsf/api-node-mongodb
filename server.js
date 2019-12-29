fcdbff105682d6a2efd52fd6141bac52e72f352d
Carlos Henrique <ch.ferreir@hotmail.com>
2019-12-29 12:19:29 -0300

    CRUD no mongodb usando get, post, put e delete

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o app
 const app =  express();
+app.use(express.json());
 
 // iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-// Promeira rota URL
-app.get('/', (req, res) => {
-    res.send('Hello Node');
-});
+//require('./src/models/Product');
+requireDir('./src/models');
+
+// importando rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
 
